c26d3e2f9e8100070b7d9ee030c88b41b481ccc1
davide <dacapoccia@gmail.com>
2022-07-20 20:49:11 +0200

    milestn2

--- main.js
+++ main.js
@@ -55,3 +55,39 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+const containerDom = document.getElementById('container');
+for (let x=0; x<posts.length; x++){
+    containerDom.innerHTML+=`<div class="post">
+                                <div class="post__header">
+                                    <div class="post-meta">                    
+                                        <div class="post-meta__icon">
+                                            <img class="profile-pic" src="${posts[x].author.image}" alt="Phil Mangione">                    
+                                        </div>
+                                        <div class="post-meta__data">
+                                            <div class="post-meta__author">${posts[x].author['name']}/div>
+                                            <div class="post-meta__time">${posts[x].created}</div>
+                                        </div>                    
+                                    </div>
+                                </div>
+                                <div class="post__text">${posts[x].content}</div>
+                                <div class="post__image">
+                                    <img src="${posts[x].media}" alt="">
+                                </div>
+                                <div class="post__footer">
+                                    <div class="likes js-likes">
+                                        <div class="likes__cta">
+                                            <a class="like-button  js-like-button" href="#" data-postid="${posts[x].id}">
+                                                <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                                                <span class="like-button__label">Mi Piace</span>
+                                            </a>
+                                        </div>
+                                        <div class="likes__counter">
+                                            Piace a <b id="like-counter-${x}" class="js-likes-counter">${posts[x].likes}</b> persone
+                                        </div>
+                                    </div> 
+                                </div>            
+                            </div>
+
+                            </div>`
+}
